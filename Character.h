1069ca897dcfcc6f1bf77a2f7da0e35804d8ef7f
Bulletbats <bulletbats4@gmail.com>
2025-05-26 14:56:28 +0930

    Update Character.h

--- Character.h
+++ Character.h
@@ -1,11 +1,10 @@
-#include <string>
-#include <vector>
-#include "Item.h"
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
+#include <string>
+#include <vector>
 #include <iostream>
-using namespace std;
+#include "Item.h"
 
 class Character {
 protected:
@@ -14,14 +13,16 @@ protected:
     int damage;
     int attack_power;
     int level;
-    std::vector<Item> inventory;  // Added inventory
+    std::vector<Item> inventory;
 
 public:
     virtual ~Character() {}
 
+    // Abstract combat functions
     virtual void attack(Character* opponent, int damage) = 0;
     virtual void takeDamage(int damage) = 0;
 
+    // Basic getters and setters
     std::string getName() const { return name; }
     void setName(const std::string& newName) { name = newName; }
 
@@ -39,17 +40,40 @@ public:
 
     bool is_alive() const { return health > 0; }
 
-    // Item methods
-    void addItem(const Item& item) { inventory.push_back(item); }
+    // Inventory commands
+    void addItem(const Item& item) {
+        inventory.push_back(item);
+        std::cout << "[INFO] " << item.get_name() << " added to inventory.\n";
+    }
 
     void useItem(int index) {
         if (index >= 0 && index < inventory.size()) {
             inventory[index].applyEffect(this);
             inventory.erase(inventory.begin() + index);
+        } else {
+            std::cout << "[ERROR] Invalid item index.\n";
         }
     }
 
-    std::vector<Item>& getInventory() { return inventory; }
+    void printInventory() const {
+        if (inventory.empty()) {
+            std::cout << "[INFO] Inventory is empty.\n";
+            return;
+        }
+        std::cout << "[Inventory]\n";
+        for (size_t i = 0; i < inventory.size(); ++i) {
+            std::cout << i + 1 << ". " << inventory[i].get_name() << " (" << inventory[i].get_type() << ", Effect: " << inventory[i].get_effect_value() << ")\n";
+        }
+    }
+
+    std::vector<Item>& getInventory() {
+        return inventory;
+    }
+
+    // Status print
+    void printStatus() const {
+        std::cout << "[Status] " << name << " - HP: " << health << ", ATK: " << attack_power << ", Level: " << level << "\n";
+    }
 };
 
 #endif
