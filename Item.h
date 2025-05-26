1267fbd961cb5b555705e06bf65cb8a107afae9f
Bulletbats <bulletbats4@gmail.com>
2025-05-26 11:25:31 +0930

    Update Item.h

--- Item.h
+++ Item.h
@@ -7,13 +7,18 @@ class Player;  // Forward declaration
 
 // Represents collectible items
 class Item {
-public:
-    std::string name;       
-    std::string type;       
-    int effectValue;        
+private:
+    std::string name;
+    std::string type;
+    int effectValue;
 
+public:
     Item(std::string name, std::string type, int effectValue);
-    void applyEffect(Player* player);  // Apply item effect to player
+    void applyEffect(Player* player);
+    std::string get_name() const { return name; }
+    std::string get_type() const { return type; }
+    int get_effect_value() const { return effectValue; }
 };
+
 
 #endif
